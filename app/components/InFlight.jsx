88d25ed16a56c4859b173cf1164cf3dd70377cf0
Joris Bontje <jorisbontje@gmail.com>
2014-09-30 15:59:17 +0200

    refactor pilotfinder

--- app/components/InFlight.jsx
+++ app/components/InFlight.jsx
@@ -3,15 +3,10 @@
 var React = require("react");
 var Router = require("react-router");
 
-var geolib = require("geolib");
-var _ = require("lodash");
-
 var FlightInfo = require("./FlightInfo");
 var FlightInfoMixin = require("./FlightInfoMixin");
 var GMap = require("./GMap");
-var PilotsList = require("./PilotsList");
-
-var MAX_LAST_SEEN = 600000;
+var PilotFinder = require("./PilotFinder");
 
 var InFlight = React.createClass({
     mixins: [FlightInfoMixin],
@@ -22,8 +17,6 @@ var InFlight = React.createClass({
 
     getInitialState: function() {
         return {
-            searching: true,
-            activePilots: [],
             flightId: null
         };
     },
@@ -31,12 +24,13 @@ var InFlight = React.createClass({
     render: function() {
         var latitude = parseFloat(this.props.params.latitude);
         var longitude = parseFloat(this.props.params.longitude);
+        var searching = this.state.flight && !this.state.flight.pilotId;
 
         return (
             <div className="client">
                 <div className="row">
                     <div className="col-xs-12">
-                        <h1>{this.state.searching ? 'SEARCHING' : 'IN FLIGHT'}</h1>
+                        <h1>{searching ? 'SEARCHING' : 'IN FLIGHT'}</h1>
                     </div>
                 </div>
                 <GMap latitude={latitude} longitude={longitude} address={this.props.params.address} showAddress={true}
@@ -44,16 +38,14 @@ var InFlight = React.createClass({
                 <div className="row">
                     <div className="col-xs-12">
                         <p>Pick up time is approximately X min.</p>
-                        {!this.state.searching && <button type="button" className="btn btn-success" onClick={this.onCall}>Call Pilot</button>}
+                        {!searching &&
+                            <button type="button" className="btn btn-success" onClick={this.onCall}>Call Pilot</button>
+                        }
                         <button type="button" className="btn btn-danger" onClick={this.onCancel}>Cancel</button>
                         <hr />
                         <FlightInfo flight={this.state.flight} />
-                        {this.state.flight && !this.state.flight.pilotId &&
-                            <div>
-                                <hr />
-                                <p>Available Pilots: {_.size(this.state.activePilots)}</p>
-                                <PilotsList pilots={this.state.activePilots} flightId={this.state.flightId} />
-                            </div>
+                        {searching &&
+                            <PilotFinder latitude={latitude} longitude={longitude} flightId={this.state.flightId} />
                         }
                     </div>
                 </div>
@@ -61,38 +53,8 @@ var InFlight = React.createClass({
         );
     },
 
-    onPilotsUpdate: function(pilots) {
-        if (this.isMounted()) {
-            var clientPos = {latitude: parseFloat(this.props.params.latitude),
-                             longitude: parseFloat(this.props.params.longitude)};
-            var now = Date.now();
-
-            var activePilots = _.chain(pilots)
-                           .mapValues(function(val, key) {
-                                val.id = key;
-                                val.age = now - val.lastSeen;
-                                if (_.has(val, 'latitude') && _.has(val, 'longitude')) {
-                                    val.distance = geolib.getDistance(clientPos, val);
-                                }
-                                return val;
-                            })
-                           .filter(function(pilot) {
-                               return pilot.online && now - pilot.lastSeen < MAX_LAST_SEEN && _.has(pilot, 'distance');
-                           })
-                           .sortBy('distance')
-                           .value();
-
-            this.setState({activePilots: activePilots});
-        }
-    },
-
     componentDidMount: function() {
-        this.context.client.listenPilots(this.onPilotsUpdate);
         this.setState({flightId: this.props.params.flightId});
-    },
-
-    componentWillUnmount: function() {
-        this.context.client.unlistenPilots();
     },
 
     onCall: function() {
---
+++ app/components/PilotFinder.jsx
@@ -0,0 +1,101 @@
+/** @jsx React.DOM */
+
+var React = require("react");
+
+var geolib = require("geolib");
+var _ = require("lodash");
+
+var MAX_LAST_SEEN = 600000;
+
+var PilotItem = React.createClass({
+    contextTypes: {
+        client: React.PropTypes.object
+    },
+
+    render: function() {
+        return (
+            <li>
+                id={this.props.pilot.id} distance={this.props.pilot.distance} lastSeen={this.props.pilot.lastSeen} age={this.props.pilot.age}
+                <button type="button" className="btn btn-success" onClick={this.onPing}>Ping</button>
+            </li>
+        );
+    },
+
+    onPing: function() {
+        console.log("ping", this.props.pilot.id);
+        this.context.client.pingPilot(this.props.pilot.id, this.props.flightId);
+    }
+});
+
+var PilotsList = React.createClass({
+    render: function() {
+        var pilotsListNodes = this.props.pilots.map(function(pilot) {
+            return (
+                <PilotItem key={pilot.id} pilot={pilot} flightId={this.props.flightId} />
+            );
+        }.bind(this));
+
+        return (
+            <ul>
+                {pilotsListNodes}
+            </ul>
+        );
+    }
+});
+
+var PilotFinder = React.createClass({
+    contextTypes: {
+        client: React.PropTypes.object
+    },
+
+    getInitialState: function() {
+        return {
+            activePilots: []
+        };
+    },
+
+    render: function() {
+        return (
+            <div>
+                <hr />
+                <p>Available Pilots: {_.size(this.state.activePilots)}</p>
+                <PilotsList pilots={this.state.activePilots} flightId={this.props.flightId} />
+            </div>
+        );
+    },
+
+    onPilotsUpdate: function(pilots) {
+        if (this.isMounted()) {
+            var clientPos = {latitude: this.props.latitude,
+                             longitude: this.props.longitude};
+            var now = Date.now();
+
+            var activePilots = _.chain(pilots)
+                           .mapValues(function(val, key) {
+                                val.id = key;
+                                val.age = now - val.lastSeen;
+                                if (_.has(val, 'latitude') && _.has(val, 'longitude')) {
+                                    val.distance = geolib.getDistance(clientPos, val);
+                                }
+                                return val;
+                            })
+                           .filter(function(pilot) {
+                               return pilot.online && now - pilot.lastSeen < MAX_LAST_SEEN && _.has(pilot, 'distance');
+                           })
+                           .sortBy('distance')
+                           .value();
+
+            this.setState({activePilots: activePilots});
+        }
+    },
+
+    componentDidMount: function() {
+        this.context.client.listenPilots(this.onPilotsUpdate);
+    },
+
+    componentWillUnmount: function() {
+        this.context.client.unlistenPilots();
+    },
+});
+
+module.exports = PilotFinder;
