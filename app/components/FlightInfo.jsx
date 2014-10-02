d7e989595934671ecf1a774b146075ee22d125ee
Joris Bontje <jorisbontje@gmail.com>
2014-10-02 10:58:20 +0200

    calculate fare

--- app/components/FlightInfo.jsx
+++ app/components/FlightInfo.jsx
@@ -5,6 +5,10 @@ var React = require("react");
 var geolib = require("geolib");
 var moment = require("moment");
 
+var BASE_FARE = 2.50;
+var MINUTE_FARE = 0.20;
+var KM_FARE = 1.10;
+
 var FlightInfo = React.createClass({
 
     render: function() {
@@ -14,10 +18,12 @@ var FlightInfo = React.createClass({
 
         var duration = null;
         var distance = null;
+        var price = null;
 
         if (this.props.flight.dropoff) {
             duration = moment(this.props.flight.dropoff.timestamp).diff(this.props.flight.pickup.timestamp, 'minutes');
-            distance = geolib.getDistance(this.props.flight.pickup, this.props.flight.dropoff);
+            distance = geolib.getDistance(this.props.flight.pickup, this.props.flight.dropoff) / 1000;
+            price = BASE_FARE + duration *  MINUTE_FARE + distance * KM_FARE;
         }
 
         return (
@@ -29,7 +35,9 @@ var FlightInfo = React.createClass({
                     {this.props.flight.dropoff &&
                         <li>Dropoff: latitude={this.props.flight.dropoff.latitude} longitude={this.props.flight.dropoff.longitude}</li>}
                     <li>Duration={duration} min</li>
-                    <li>Distance={distance} meters</li>
+                    <li>Distance={distance.toFixed(1)} km</li>
+                    {price &&
+                        <li>Price={price.toFixed(2)}</li>}
                     <li>Client={this.props.flight.clientId}</li>
                     <li>Pilot={this.props.flight.pilotId}</li>
                 </ul>
